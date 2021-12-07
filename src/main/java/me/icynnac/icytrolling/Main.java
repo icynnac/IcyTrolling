88cb0d787f34e80282237f7b5b08b1d86de95aeb
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-06 22:08:42 -0500

    updated Main.java for some commands

--- src/main/java/me/icynnac/icytrolling/Main.java
+++ src/main/java/me/icynnac/icytrolling/Main.java
@@ -1,9 +1,8 @@
 package me.icynnac.icytrolling;
 
-import me.icynnac.icytrolling.commands.drop;
-import me.icynnac.icytrolling.commands.explode;
-import me.icynnac.icytrolling.commands.fire;
+import me.icynnac.icytrolling.commands.*;
 import me.icynnac.icytrolling.utils.tabcompleters.dropTab;
+import me.icynnac.icytrolling.utils.tabcompleters.pumpkinTab;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,5 +23,12 @@ public final class Main extends JavaPlugin {
         Objects.requireNonNull(getCommand("drop")).setExecutor(new drop());
         Objects.requireNonNull(getCommand("drop")).setPermissionMessage("no.");
         Objects.requireNonNull(getCommand("drop")).setTabCompleter(new dropTab());
+        Objects.requireNonNull(getCommand("pumpkin")).setExecutor(new pumpkin());
+        Objects.requireNonNull(getCommand("pumpkin")).setPermissionMessage("no.");
+        Objects.requireNonNull(getCommand("pumpkin")).setTabCompleter(new pumpkinTab());
+        Objects.requireNonNull(getCommand("fling")).setExecutor(new fling());
+        Objects.requireNonNull(getCommand("fling")).setPermissionMessage("no.");
+        Objects.requireNonNull(getCommand("levitate")).setExecutor(new fly());
+        Objects.requireNonNull(getCommand("levitate")).setPermissionMessage("no.");
     }
 }

de87526b20be25128bf34ede7789150446c157e8
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-16 10:45:38 -0500

    Reworked /levitate

--- src/main/java/me/icynnac/icytrolling/commands/fly.java
+++ src/main/java/me/icynnac/icytrolling/commands/fly.java
@@ -1,6 +1,5 @@
 package me.icynnac.icytrolling.commands;
 
-import me.icynnac.icytrolling.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -18,20 +17,72 @@ public class fly implements CommandExecutor {
             if (args.length > 0) {
                 Player t = Bukkit.getPlayer(args[0]);
                 if (t != null) {
-                    if (!t.hasPotionEffect(PotionEffectType.LEVITATION)) {
-                        t.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 999999, Main.instance.getConfig().getInt("commands.levitation-power")));
-                        t.sendMessage(ChatColor.DARK_AQUA + "Whoops, you're going to the sun!");
-                        sender.sendMessage(ChatColor.DARK_AQUA + "Sent " + ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " to the sun.");
-                    } else {
-                        t.removePotionEffect(PotionEffectType.LEVITATION);
-                        t.sendMessage(ChatColor.DARK_AQUA + "Whoops, your falling.");
-                        sender.sendMessage(ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " is now coming back down...");
+                    if (args.length > 1) {
+                        switch (args[1].toLowerCase()) {
+                            case "1":
+                                if (!t.hasPotionEffect(PotionEffectType.LEVITATION)) {
+                                    t.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 999999, 1));
+                                    t.sendMessage(ChatColor.DARK_AQUA + "Whoops, you're going to the sun!");
+                                    sender.sendMessage(ChatColor.DARK_AQUA + "Sent " + ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " to the sun.");
+                                } else {
+                                    sender.sendMessage(ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " already has levitation.");
+                                }
+                                break;
+                            case "2":
+                                if (!t.hasPotionEffect(PotionEffectType.LEVITATION)) {
+                                    t.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 999999, 2));
+                                    t.sendMessage(ChatColor.DARK_AQUA + "Whoops, you're going to the sun!");
+                                    sender.sendMessage(ChatColor.DARK_AQUA + "Sent " + ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " to the sun.");
+                                } else {
+                                    sender.sendMessage(ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " already has levitation.");
+                                }
+                                break;
+                            case "3":
+                                if (!t.hasPotionEffect(PotionEffectType.LEVITATION)) {
+                                    t.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 999999, 3));
+                                    t.sendMessage(ChatColor.DARK_AQUA + "Whoops, you're going to the sun!");
+                                    sender.sendMessage(ChatColor.DARK_AQUA + "Sent " + ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " to the sun.");
+                                } else {
+                                    sender.sendMessage(ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " already has levitation.");
+                                }
+                                break;
+                            case "4":
+                                if (!t.hasPotionEffect(PotionEffectType.LEVITATION)) {
+                                    t.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 999999, 4));
+                                    t.sendMessage(ChatColor.DARK_AQUA + "Whoops, you're going to the sun!");
+                                    sender.sendMessage(ChatColor.DARK_AQUA + "Sent " + ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " to the sun.");
+                                } else {
+                                    sender.sendMessage(ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " already has levitation.");
+                                }
+                                break;
+                            case "5":
+                                if (!t.hasPotionEffect(PotionEffectType.LEVITATION)) {
+                                    t.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 999999, 5));
+                                    t.sendMessage(ChatColor.DARK_AQUA + "Whoops, you're going to the sun!");
+                                    sender.sendMessage(ChatColor.DARK_AQUA + "Sent " + ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " to the sun.");
+                                } else {
+                                    sender.sendMessage(ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " already has levitation.");
+                                }
+                                break;
+                            case "off":
+                                if (t.hasPotionEffect(PotionEffectType.LEVITATION)) {
+                                    t.removePotionEffect(PotionEffectType.LEVITATION);
+                                    t.sendMessage(ChatColor.DARK_AQUA + "Whoops, your falling.");
+                                    sender.sendMessage(ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " is now coming back down...");
+                                } else {
+                                    sender.sendMessage(ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " doesn't have levitation.");
+                                }
+                                break;
+                            default:
+                                sender.sendMessage(ChatColor.RED + "That command doesn't look right, try: /levitate (target's username) (effect power)");
+                                break;
+                        }
                     }
                 } else {
                     sender.sendMessage(ChatColor.RED + "That player doesn't exist, did you make a typo?");
                 }
             } else {
-                sender.sendMessage(ChatColor.RED + "That command doesn't look right, try: /levitate (target's username)");
+                sender.sendMessage(ChatColor.RED + "That command doesn't look right, try: /levitate (target's username) (effect power)");
             }
         }
         return false;

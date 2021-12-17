5b82d24acc3fd048b288ec54b644bc4e0097f949
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-16 19:16:29 -0500

    Reworked /fling

--- src/main/java/me/icynnac/icytrolling/commands/fling.java
+++ src/main/java/me/icynnac/icytrolling/commands/fling.java
@@ -1,5 +1,6 @@
 package me.icynnac.icytrolling.commands;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -17,19 +18,29 @@ public class fling implements CommandExecutor {
             if (args.length > 0) {
                 Player t = Bukkit.getPlayer(args[0]);
                 if (t != null) {
-                    Location loc = t.getLocation();
-                    Vector center = loc.toVector();
-                    Vector toThrow = loc.toVector();
-                    double x = toThrow.getX() - center.getX();
-                    double z = toThrow.getZ() - center.getZ();
-                    Vector v = new Vector(x, 1, z).normalize().multiply(3);
-                    t.setVelocity(v);
-                    sender.sendMessage(ChatColor.DARK_AQUA + "You flung " + ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + "!");
+                    if (args.length > 1) {
+                        if (StringUtils.isNumeric(args[1])) {
+                            if (!(Integer.parseInt(args[1]) > 4)) {
+                                Location loc = t.getLocation();
+                                Vector center = loc.toVector();
+                                Vector toThrow = loc.toVector();
+                                double x = toThrow.getX() - center.getX();
+                                double z = toThrow.getZ() - center.getZ();
+                                Vector v = new Vector(x, 1, z).normalize().multiply(Integer.parseInt(args[1]));
+                                t.setVelocity(v);
+                                sender.sendMessage(ChatColor.DARK_AQUA + "You flung " + ChatColor.AQUA + t.getName() + ChatColor.DARK_AQUA + " with a multiplier of " + ChatColor.AQUA + Integer.parseInt(args[1]) + ChatColor.DARK_AQUA + "!");
+                            } else {
+                                sender.sendMessage(ChatColor.RED + "Fling multiplier cannot be bigger than four.");
+                            }
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "That command doesn't look right, try: /fling (target's username) (multiplier)");
+                        }
+                    }
                 } else {
                     sender.sendMessage(ChatColor.RED + "That player doesn't exist, did you make a typo?");
                 }
             } else {
-                sender.sendMessage(ChatColor.RED + "That command doesn't look right, try: /fling (target's username)");
+                sender.sendMessage(ChatColor.RED + "That command doesn't look right, try: /fling (target's username) (multiplier)");
             }
         }
         return false;

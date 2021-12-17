c631a702c9326b2f5d553e112760ffa65fa85e15
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-16 19:39:37 -0500

    @NotNull

--- src/main/java/me/icynnac/icytrolling/utils/tabcompleters/pumpkinTab.java
+++ src/main/java/me/icynnac/icytrolling/utils/tabcompleters/pumpkinTab.java
@@ -3,6 +3,7 @@ package me.icynnac.icytrolling.utils.tabcompleters;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
 public class pumpkinTab implements TabCompleter {
     List<String> arg = new ArrayList<>();
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
         if (arg.isEmpty()) {
             arg.add("on"); arg.add("off");
         }

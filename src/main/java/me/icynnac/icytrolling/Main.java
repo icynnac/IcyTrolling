e9ed3fa601feff1932a885a9484261900246aeab
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-17 22:57:58 -0500

    config.yml global variables

--- src/main/java/me/icynnac/icytrolling/Main.java
+++ src/main/java/me/icynnac/icytrolling/Main.java
@@ -10,15 +10,41 @@ import java.util.Objects;
 public final class Main extends JavaPlugin {
 
     public static Plugin instance;
+    
+    public static String prefix;
+    public static String noplr;
+
+    public static String badcmddemo;
+    public static String badcmddrop;
+    public static String badcmdfire;
+    public static String badcmdboom;
+    public static String badcmdfly;
+    public static String badcmdfling;
+    public static String badcmdlag;
+    public static String badcmdpumpkin;
 
     @Override
     public void onEnable() {
         instance = this;
+        prefix = this.getConfig().getString("messages.prefix");
+        noplr = this.getConfig().getString("messages.invalid-player");
 
+        badcmddemo = this.getConfig().getString("messages.invalid-cmd-demo");
+        badcmddrop = this.getConfig().getString("messages.invalid-cmd-drop");
+        badcmdboom = this.getConfig().getString("messages.invalid-cmd-explode");
+        badcmdfire = this.getConfig().getString("messages.invalid-cmd-fire");
+        badcmdfly = this.getConfig().getString("messages.invalid-cmd-levitate");
+        badcmdfling = this.getConfig().getString("messages.invalid-cmd-fling");
+        badcmdlag = this.getConfig().getString("messages.invalid-cmd-lag");
+        badcmdpumpkin = this.getConfig().getString("messages.invalid-cmd-pumpkin");
+
+        Objects.requireNonNull(getCommand("icytroll")).setExecutor(new icytroll());
+        Objects.requireNonNull(getCommand("icytroll")).setPermissionMessage("no.");
+        Objects.requireNonNull(getCommand("icytroll")).setTabCompleter(new icytrollTab());
         Objects.requireNonNull(getCommand("explode")).setExecutor(new explode());
         Objects.requireNonNull(getCommand("explode")).setPermissionMessage("no.");
         Objects.requireNonNull(getCommand("explode")).setTabCompleter(new explodeTab());
-        Objects.requireNonNull(getCommand("burn")).setExecutor(new fire());
+        Objects.requireNonNull(getCommand("burn")).setExecutor(new burn());
         Objects.requireNonNull(getCommand("burn")).setPermissionMessage("no.");
         Objects.requireNonNull(getCommand("drop")).setExecutor(new drop());
         Objects.requireNonNull(getCommand("drop")).setPermissionMessage("no.");
@@ -28,7 +54,7 @@ public final class Main extends JavaPlugin {
         Objects.requireNonNull(getCommand("pumpkin")).setTabCompleter(new pumpkinTab());
         Objects.requireNonNull(getCommand("fling")).setExecutor(new fling());
         Objects.requireNonNull(getCommand("fling")).setPermissionMessage("no.");
-        Objects.requireNonNull(getCommand("levitate")).setExecutor(new fly());
+        Objects.requireNonNull(getCommand("levitate")).setExecutor(new levitate());
         Objects.requireNonNull(getCommand("levitate")).setPermissionMessage("no.");
         Objects.requireNonNull(getCommand("levitate")).setTabCompleter(new flyTab());
         Objects.requireNonNull(getCommand("lag")).setExecutor(new lag());
@@ -36,5 +62,7 @@ public final class Main extends JavaPlugin {
         Objects.requireNonNull(getCommand("lag")).setTabCompleter(new lagTab());
         Objects.requireNonNull(getCommand("demo")).setExecutor(new demo());
         Objects.requireNonNull(getCommand("demo")).setPermissionMessage("no.");
+
+        saveDefaultConfig();
     }
 }

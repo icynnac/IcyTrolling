9132f6df91871ad84e22331ee30bd8e7b352350f
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-23 16:15:58 -0500

    Completely removed TabCompleters, added other utils

---
+++ src/main/java/me/icynnac/icytrolling/utils/InvalidCommand.java
@@ -0,0 +1,35 @@
+package me.icynnac.icytrolling.utils;
+
+import me.icynnac.icytrolling.Main;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+import static me.icynnac.icytrolling.Main.plugin;
+
+public enum InvalidCommand {
+    NO_PLAYER(0),
+    DEMO(1),
+    DROP(2),
+    EXPLODE(3),
+    FIRE(4),
+    LEVITATE(5),
+    FLING(6),
+    LAG(7),
+    PUMPKIN(8),
+    SCAM(9),
+    CONFIG(10);
+
+    public final int id;
+
+    InvalidCommand(int id) { this.id = id; }
+
+    public boolean sendMessage(CommandSender sender) {
+        StringBuilder sb = new StringBuilder(Main.prefix + " ");
+        String node = "";
+        if (id > 0) node = ("messages.invalid-cmd-" + this.toString().toLowerCase());
+        else if (id == 0) node = "messages.invalid-player";
+        sb.append(plugin.getConfig().getString(node));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', sb.toString()));
+        return true;
+    }
+}

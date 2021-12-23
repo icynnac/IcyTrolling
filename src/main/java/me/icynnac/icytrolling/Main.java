1f15a9653e4bf5f92199ad576809091e4f6b62cb
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-23 16:16:41 -0500

    Neater Main.java, with an update checker

--- src/main/java/me/icynnac/icytrolling/Main.java
+++ src/main/java/me/icynnac/icytrolling/Main.java
@@ -1,69 +1,68 @@
 package me.icynnac.icytrolling;
 
 import me.icynnac.icytrolling.commands.*;
-import me.icynnac.icytrolling.utils.tabcompleters.*;
-import org.bukkit.plugin.Plugin;
+import me.icynnac.icytrolling.events.PlayerEvents;
+import me.icynnac.icytrolling.events.UIEvents;
+import me.icynnac.icytrolling.utils.UpdateChecker;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.UUID;
 
 public final class Main extends JavaPlugin {
 
-    public static Plugin instance;
-    
-    public static String prefix;
-    public static String noplr;
+    public static Main plugin;
 
-    public static String badcmddemo;
-    public static String badcmddrop;
-    public static String badcmdfire;
-    public static String badcmdboom;
-    public static String badcmdfly;
-    public static String badcmdfling;
-    public static String badcmdlag;
-    public static String badcmdpumpkin;
+    public static HashMap<UUID, ItemStack> playerPumpkinHeads = new HashMap<>();
+    public static String prefix;
 
     @Override
     public void onEnable() {
-        instance = this;
-        prefix = this.getConfig().getString("messages.prefix");
-        noplr = this.getConfig().getString("messages.invalid-player");
+        plugin = this;
+        prefix = ChatColor.translateAlternateColorCodes('&', getConfig().getString("messages.prefix"));
 
-        badcmddemo = this.getConfig().getString("messages.invalid-cmd-demo");
-        badcmddrop = this.getConfig().getString("messages.invalid-cmd-drop");
-        badcmdboom = this.getConfig().getString("messages.invalid-cmd-explode");
-        badcmdfire = this.getConfig().getString("messages.invalid-cmd-fire");
-        badcmdfly = this.getConfig().getString("messages.invalid-cmd-levitate");
-        badcmdfling = this.getConfig().getString("messages.invalid-cmd-fling");
-        badcmdlag = this.getConfig().getString("messages.invalid-cmd-lag");
-        badcmdpumpkin = this.getConfig().getString("messages.invalid-cmd-pumpkin");
+        Bukkit.getPluginManager().registerEvents(new UIEvents(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerEvents(), this);
 
-        Objects.requireNonNull(getCommand("icytroll")).setExecutor(new icytroll());
-        Objects.requireNonNull(getCommand("icytroll")).setPermissionMessage("no.");
-        Objects.requireNonNull(getCommand("icytroll")).setTabCompleter(new icytrollTab());
-        Objects.requireNonNull(getCommand("explode")).setExecutor(new explode());
-        Objects.requireNonNull(getCommand("explode")).setPermissionMessage("no.");
-        Objects.requireNonNull(getCommand("explode")).setTabCompleter(new explodeTab());
-        Objects.requireNonNull(getCommand("burn")).setExecutor(new burn());
-        Objects.requireNonNull(getCommand("burn")).setPermissionMessage("no.");
-        Objects.requireNonNull(getCommand("drop")).setExecutor(new drop());
-        Objects.requireNonNull(getCommand("drop")).setPermissionMessage("no.");
-        Objects.requireNonNull(getCommand("drop")).setTabCompleter(new dropTab());
-        Objects.requireNonNull(getCommand("pumpkin")).setExecutor(new pumpkin());
-        Objects.requireNonNull(getCommand("pumpkin")).setPermissionMessage("no.");
-        Objects.requireNonNull(getCommand("pumpkin")).setTabCompleter(new pumpkinTab());
-        Objects.requireNonNull(getCommand("fling")).setExecutor(new fling());
-        Objects.requireNonNull(getCommand("fling")).setPermissionMessage("no.");
-        Objects.requireNonNull(getCommand("levitate")).setExecutor(new levitate());
-        Objects.requireNonNull(getCommand("levitate")).setPermissionMessage("no.");
-        Objects.requireNonNull(getCommand("levitate")).setTabCompleter(new flyTab());
-        Objects.requireNonNull(getCommand("lag")).setExecutor(new lag());
-        Objects.requireNonNull(getCommand("lag")).setPermissionMessage("no.");
-        Objects.requireNonNull(getCommand("lag")).setTabCompleter(new lagTab());
-        Objects.requireNonNull(getCommand("demo")).setExecutor(new demo());
-        Objects.requireNonNull(getCommand("demo")).setPermissionMessage("no.");
+        getCommand("levitate").setPermissionMessage("no.");
+        getCommand("icytroll").setPermissionMessage("no.");
+        getCommand("explode").setPermissionMessage("no.");
+        getCommand("pumpkin").setPermissionMessage("no.");
+        getCommand("fling").setPermissionMessage("no.");
+        getCommand("demo").setPermissionMessage("no.");
+        getCommand("burn").setPermissionMessage("no.");
+        getCommand("drop").setPermissionMessage("no.");
+        getCommand("lag").setPermissionMessage("no.");
+        getCommand("scam").setPermissionMessage("no.");
+
+        getCommand("levitate").setExecutor(new CmdLevitate());
+        getCommand("icytroll").setExecutor(new CmdIcyTroll());
+        getCommand("explode").setExecutor(new CmdExplode());
+        getCommand("pumpkin").setExecutor(new CmdPumpkin());
+        getCommand("fling").setExecutor(new CmdFling());
+        getCommand("demo").setExecutor(new CmdDemo());
+        getCommand("burn").setExecutor(new CmdBurn());
+        getCommand("drop").setExecutor(new CmdDrop());
+        getCommand("lag").setExecutor(new CmdLag());
+        getCommand("scam").setExecutor(new CmdScam());
 
         saveDefaultConfig();
 
+        new UpdateChecker(this, 98539).getVersion(version -> {
+            if (!this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                Bukkit.getConsoleSender().sendMessage("§a§lIcy§3§lTrolling §8§l>> §eThere's a new version available! Head to the spigot page (/icytroll info) to get the latest version.");
+            } else {
+                Bukkit.getConsoleSender().sendMessage("§a§lIcy§3§lTrolling §8§l>> §aYou're on the latest version.");
+            }
+        });
+    }
+
+    @Override
+    public void onDisable() {
+        for (UUID uuid : playerPumpkinHeads.keySet())
+            Bukkit.getOfflinePlayer(uuid).getPlayer().getEquipment().setHelmet(playerPumpkinHeads.get(uuid));
     }
 }

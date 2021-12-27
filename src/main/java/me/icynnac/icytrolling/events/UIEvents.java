d73140cec90539bbd6f405cdfd703f2158cb61b0
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-27 16:19:30 -0500

    Added 2 more random events in /scam

--- src/main/java/me/icynnac/icytrolling/events/UIEvents.java
+++ src/main/java/me/icynnac/icytrolling/events/UIEvents.java
@@ -1,16 +1,19 @@
 package me.icynnac.icytrolling.events;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.Random;
+
+import static me.icynnac.icytrolling.Main.playerDecreasedHealth;
 
 public class UIEvents implements Listener {
     @EventHandler
@@ -20,7 +23,7 @@ public class UIEvents implements Listener {
             e.setCancelled(true);
             if (e.getCurrentItem().getType() == Material.EMERALD) {
                 Random random = new Random();
-                int rand_int = random.nextInt(3);
+                int rand_int = random.nextInt(5);
                 switch (rand_int) {
                     case 0:
                         player.closeInventory();
@@ -36,6 +39,21 @@ public class UIEvents implements Listener {
                     case 2:
                         player.closeInventory();
                         player.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 100000, 3));
+                        player.sendMessage("§cGet scammed noob.");
+                        break;
+                    case 3:
+                        player.closeInventory();
+                        ItemStack epicdirt = new ItemStack(Material.DIRT);
+                        ItemMeta ed = epicdirt.getItemMeta();
+                        ed.setDisplayName("§aEmerald");
+                        epicdirt.setItemMeta(ed);
+                        player.getInventory().addItem(epicdirt);
+                        player.sendMessage("§cGet scammed noob.");
+                        break;
+                    case 4:
+                        player.closeInventory();
+                        player.setMaxHealth(2);
+                        playerDecreasedHealth.put(player.getUniqueId(), true);
                         player.sendMessage("§cGet scammed noob.");
                         break;
                 }

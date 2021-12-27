4c974bf4ad70cfcc8172b8ba29043034cd73d726
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-27 16:24:30 -0500

    Changed /burn length from ticks to seconds

--- src/main/java/me/icynnac/icytrolling/commands/CmdBurn.java
+++ src/main/java/me/icynnac/icytrolling/commands/CmdBurn.java
@@ -22,7 +22,8 @@ public class CmdBurn implements TabExecutor {
                 if (t != null) {
                     if (args.length > 1) {
                         if (StringUtils.isNumeric(args[1])) {
-                            t.setFireTicks(Integer.parseInt(args[1]));
+                            int firelength = Integer.parseInt(args[1]) * 20;
+                            t.setFireTicks(firelength);
                             sender.sendMessage("§b" + t.getName() + "§3 is now on fire for §b" + args[1] + "§3 ticks.");
                         } else return InvalidCommand.FIRE.sendMessage(sender);
                     } else return InvalidCommand.FIRE.sendMessage(sender);
@@ -34,7 +35,7 @@ public class CmdBurn implements TabExecutor {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
         if (args.length == 1) return null;
-        if (args.length == 2) return new ArrayList<>(Collections.singletonList("<length>"));
+        if (args.length == 2) return new ArrayList<>(Collections.singletonList("<Length in Seconds>"));
         return new ArrayList<>();
     }
 }

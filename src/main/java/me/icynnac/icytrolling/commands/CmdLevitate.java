f17deb47d2b089e7876318770a2b42977deca2a1
icynnac <68450404+icynnac@users.noreply.github.com>
2021-12-27 16:36:11 -0500

    Increased levitate power to 10

--- src/main/java/me/icynnac/icytrolling/commands/CmdLevitate.java
+++ src/main/java/me/icynnac/icytrolling/commands/CmdLevitate.java
@@ -25,30 +25,30 @@ public class CmdLevitate implements TabExecutor {
                     if (args.length > 1) {
                         if (StringUtils.isNumeric(args[1])) {
                             int num = Integer.parseInt(args[1]);
-                            if (0 < num && num < 5) {
+                            if (0 < num && num < 11) {
                                 if (!t.hasPotionEffect(PotionEffectType.LEVITATION)) {
                                     t.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 99999999, num - 1));
                                     t.sendMessage("§3Whoops, you're going to the sun!");
                                     sender.sendMessage("§3Sent " + "§b" + t.getName() + "§3 to the sun.");
                                 } else sender.sendMessage("§b" + t.getName() + "§3 already has levitation.");
-                            } else sender.sendMessage("§cLevitation multiplier cannot be less than one or bigger than five.");
+                            } else sender.sendMessage("§cLevitation multiplier cannot be less than 1 or bigger than 10.");
                         } else if (args[1].equalsIgnoreCase("off")) {
                             if (t.hasPotionEffect(PotionEffectType.LEVITATION)) {
                                 t.removePotionEffect(PotionEffectType.LEVITATION);
                                 t.sendMessage("§3Whoops, you're falling.");
                                 sender.sendMessage("§b" + t.getName() + "§3 is now coming back down...");
                             } else sender.sendMessage("§b" + t.getName() + "§3 doesn't have levitation.");
-                        } else InvalidCommand.LEVITATE.sendMessage(sender);
-                    } else InvalidCommand.LEVITATE.sendMessage(sender);
-                } else InvalidCommand.NO_PLAYER.sendMessage(sender);
-            } else InvalidCommand.LEVITATE.sendMessage(sender);
+                        } else return InvalidCommand.LEVITATE.sendMessage(sender);
+                    } else return InvalidCommand.LEVITATE.sendMessage(sender);
+                } else return InvalidCommand.NO_PLAYER.sendMessage(sender);
+            } else return InvalidCommand.LEVITATE.sendMessage(sender);
         } return true;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 1) return null;
-        if (args.length == 2) return new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "off"));
+        if (args.length == 2) return new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "off"));
         return new ArrayList<>();
     }
 }
